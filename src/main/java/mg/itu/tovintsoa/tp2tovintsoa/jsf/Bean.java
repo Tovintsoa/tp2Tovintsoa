aa33ed1f476176dba106b1b28dd02c9518fc818f
Tovintsoa <tovintsoa1337@capri-nc.com>
2023-03-31 09:47:27 +0300

    TP 2

---
+++ src/main/java/mg/itu/tovintsoa/tp2tovintsoa/jsf/Bean.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tovintsoa.tp2tovintsoa.jsf;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.Dependent;
+import jakarta.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Tovintsoa-Capri
+ */
+@Named(value = "bean")
+@RequestScoped
+public class Bean {
+
+    private int nombre;
+    /**
+     * Creates a new instance of Bean
+     */
+    public Bean() {
+    }
+    public int getNombre(){
+        return nombre;
+    }
+    public void setNombre(int nombre){
+        this.nombre = nombre;
+    }
+    public List<Integer> getNombresSuivants(){
+        int nb  = 5;
+        List<Integer> l = new ArrayList<>(nb );
+        for(int i = nombre; i < nombre + nb ; i++){
+            l.add(i);
+        }
+        return l;
+    }
+    public String afficher() {
+        return "affichage_3?nb="+ nombre + "&amp;faces-redirect=true";
+    }
+    
+}
---
+++ src/main/java/mg/itu/tovintsoa/tp2tovintsoa/jsf/Bean_4.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tovintsoa.tp2tovintsoa.jsf;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.Dependent;
+import jakarta.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Tovintsoa-Capri
+ */
+@Named(value = "bean_4")
+@RequestScoped
+public class Bean_4 {
+    
+     private int nombre;
+    /**
+     * Creates a new instance of Bean_4
+     */
+    public Bean_4() {
+    }
+    
+     public int getNombre(){
+        return nombre;
+    }
+    public void setNombre(int nombre){
+        this.nombre = nombre;
+    }
+    public List<Integer> getNombresSuivants(){
+        int nb  = 5;
+        List<Integer> l = new ArrayList<>(nb );
+        for(int i = nombre; i < nombre + nb ; i++){
+            l.add(i);
+        }
+        return l;
+    }
+    public String afficher() {
+        return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
+    }
+    public String afficherValeurFormulaire(){
+         return "formulaire_4?nb="+ nombre + "&amp;faces-redirect=true";
+    }
+}
